c2cbdb2439b68b338b18a1d8c2c64fe939786280
XxThunderBlastxX <koustavmondal55@gmail.com>
2022-01-26 02:12:20 +0530

    changed to ChangeNotifierProvider.Value

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -12,8 +12,8 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider(
-        create: (context) => products[i],
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        value: products[i],
         child: ProductItem(
             // products[i].id as String,
             // products[i].title as String,
