c5ff57daaf1fe3d36742e189546d490efdf16723
XxThunderBlastxX <koustavmondal55@gmail.com>
2022-01-26 02:43:38 +0530

    added consumer to favouriteIcon

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -31,13 +31,15 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            color: Theme.of(context).accentColor,
-            onPressed: () {
-              product.toggleFavoriteStatus();
-            },
+          leading: Consumer<Product>(
+            builder: (context, product, _) => IconButton(
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              color: Theme.of(context).accentColor,
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
+            ),
           ),
           title: Text(
             product.title.toString(),
